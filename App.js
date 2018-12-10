f3bb76280fae389fee9bf527320aa06ed80c53e7
Joseph Spear <jcspear90@gmail.com>
2018-12-10 15:38:39 -0800

    Added Scoreboard and fixed Game Outcomes

--- App.js
+++ App.js
@@ -1,47 +1,60 @@
-
+//Game State Global Variables
 var turn = 'X';
 var clickedPlaces = [];
 var playerPlaces = { X:[], O:[] };
+var gamesWon = { X: 0, O: 0 };
 
+//Click handler
 var togglePlace = (event) => {
-    
     if (!clickedPlaces.includes(event.target.id)) {
         event.target.textContent = turn;
         clickedPlaces.push(event.target.id);
         playerPlaces[turn].push(event.target.id);
-        checkGameStatus(turn);
         turn = turn === 'X' ? 'O' : 'X';
         document.getElementById('current-player').textContent = turn;
+        checkGameStatus();
     }
 }
-
-var checkGameStatus = (turn) => {
-    var rows = { 1:3, 2:3, 3:3 };
-    var columns = { 1:3, 2:3, 3:3 };
-    var diagonal1 = ['1-1','2-2','3-3'];
-    var diagonal2 = ['3-1','2-2','1-3'];
-    var diagonal1Total = 3;
-    var diagonal2Total = 3;
+//Check Game State
+var checkGameStatus = () => {
+    for (var player in playerPlaces) {
+        var rows = { 1:3, 2:3, 3:3 };
+        var columns = { 1:3, 2:3, 3:3 };
+        var diagonal1 = ['1-1','2-2','3-3'];
+        var diagonal2 = ['3-1','2-2','1-3'];
+        var diagonal1Total = 3;
+        var diagonal2Total = 3;
     
-    playerPlaces[turn].forEach(place => {
-        row = place.split('-')[0];
-        col = place.split('-')[1];
-        rows[row]--;
-        columns[col]--;
-        if (diagonal1.includes(place)) { diagonal1Total--;};
-        if (diagonal2.includes(place)) { diagonal2Total--;};
-        if (anyZero(rows[row], columns[col], diagonal1Total, diagonal2Total)) {
-            setTimeout(() => endGame(turn), 250);
-        }
-    });
-
+        playerPlaces[player].forEach(place => {
+            row = place.split('-')[0];
+            col = place.split('-')[1];
+            rows[row]--;
+            columns[col]--;
+            if (diagonal1.includes(place)) { diagonal1Total--;};
+            if (diagonal2.includes(place)) { diagonal2Total--;};
+            if (anyZero(rows[row], columns[col], diagonal1Total, diagonal2Total)) {
+                turn = player;
+                endGame(player);
+                gamesWon[player]++;
+                document.getElementById(`${player}-games`).textContent = `${player}: ${gamesWon[player]}`;
+                return;
+            }
+        });
+    }
+    if (clickedPlaces.length === 9) {
+        endGame();
+    }
 }
-
+//Display game outcome
 var endGame = (turn) => {
-    alert(turn + ' wins!');
+    if (turn) {
+        alert(turn + ' wins!');
+    } else {
+        alert('Tie Game');
+    }
     clearBoard();
 }
-
+//Reset all values
 var clearBoard = () => {
     var places = document.getElementsByClassName('col');
     document.getElementById('current-player').textContent = 'X'
@@ -52,7 +65,7 @@ var clearBoard = () => {
         places[i].textContent = '';
     };
 }
-
+//Helper function to check if any arguments are zero
 var anyZero = function () {
     var output = false;
     [...arguments].forEach(el => {
